a97ba6ab01141974f06d6006d70bdc7a26b26f1a
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2025-10-28 00:17:08 +0530

    Delete Modules:Hierarchy directory

--- Modules:Hierarchy/Module_shift.v
+++
@@ -1,6 +0,0 @@
-module top_module ( input clk, input d, output q );
-    wire q1,q2;
-    my_dff inst0( clk, d, q1 );
-    my_dff inst1( clk, q1, q2 );
-    my_dff inst2( clk, q2, q );
-endmodule
--- Modules:Hierarchy/module.v
+++
@@ -1,3 +0,0 @@
-module top_module ( input a, input b, output out );
-    mod_a inst0(a,b,out);
-endmodule
