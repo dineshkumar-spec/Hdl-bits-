1be8bbde1beb0b693abef8f53545c231e6a3c94d
Dineshumar Periyasamy <dineshkumarperiyasamy11@gmail.com>
2025-11-07 22:40:18 +0530

    Create 15.Detect an edge.v

---
+++ 3. Circuits/3.2 Sequential Logic/1.Latches and Flip-Flops/15.Detect an edge.v
@@ -0,0 +1,10 @@
+module top_module (
+    input clk,
+    input [7:0] in,
+    output [7:0] pedge
+);  reg[7:0]temp;
+    always@(posedge clk)begin
+        pedge<=in&~temp;
+        temp<=in;
+    end
+endmodule
